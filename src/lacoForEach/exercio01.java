632404888ec740f08a6e54b6e977046553540161
caiodevzz <caiomateus270@gmail.com>
2026-06-18 19:24:05 -0300

    novos exercios de for each

---
+++ src/lacoForEach/exercio01.java
@@ -0,0 +1,13 @@
+package lacoForEach;
+
+public class exercio01 {
+    public static void main(String[] args) {
+
+        String[] nomes = {"Caio", "Mateus", "Dantas", "Silva"};
+
+        for (String obj : nomes) {
+            System.out.println(obj);
+        }
+
+    }
+}
