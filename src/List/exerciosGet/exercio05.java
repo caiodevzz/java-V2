e178eab71df554216cdabc87d1b8e3536a06e68d
caiodevzz <caiomateus270@gmail.com>
2026-06-27 14:32:23 -0300

    Mais exercios sobre get()

--- src/List/exerciosGet/exercio05.java
+++ src/List/exerciosGet/exercio05.java
@@ -1,4 +1,29 @@
 package List.exerciosGet;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class exercio05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> nomes = new ArrayList<>();
+
+        System.out.println("Quantas pessoas voce vai digitar ? ");
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n ; i++) {
+            System.out.println("Digite o " + (i + 1));
+            System.out.print("nome: ");
+
+            String nome = sc.next();
+            nomes.add(nome);
+        }
+
+        System.out.print("Primeiro elemento: ");
+        System.out.println(nomes.get(0));
+        System.out.print("Ultimo elemento: ");
+        System.out.println(nomes.get(nomes.size() -1));
+    }
 }
