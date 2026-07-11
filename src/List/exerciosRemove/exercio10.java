7217ee2fc1e2c5ee216407a283d57081dc678762
caiodevzz <caiomateus270@gmail.com>
2026-07-11 10:07:09 -0300

    commit diario

---
+++ src/List/exerciosRemove/exercio10.java
@@ -0,0 +1,21 @@
+package List.exerciosRemove;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class exercio10 {
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(2);
+
+        numeros.remove(2);
+
+        System.out.println(numeros);
+    }
+}
